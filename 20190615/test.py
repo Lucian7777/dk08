e4e4d5a71dd970bfc1c63bcb8eb57e3fce51b72b
郭玉生 <41769663+cngys@users.noreply.github.com>
2019-06-17 08:14:00 +0800

    Update test.py

--- 20190615/test.py
+++ 20190615/test.py
@@ -7,11 +7,9 @@ import matplotlib.pyplot as plt
 #3.09数据处理以及时间数据初始化
 data_path = "G:\dataset\data_1010309.mat"
 data = scio.loadmat(data_path)
-print(data)
 data_ar = data.get('data_train_1010309')
 X = np.arange(1440)[:, None]
 y = np.zeros(1440)
-print(X, y)
 for i in X:
     y[i] = data_ar[i, 1]
 Xt = np.linspace(0, 1439, 1440)[:, None]
@@ -50,9 +48,9 @@ def display_result(time, value, postion, title_name, col):
 display_result(Xt, y, 331, 'Section_1010309', 'blue')
 display_result(Xt, yt, 331, 'Section_1010309', 'red')
 display_result(Xt, y_0310, 332, 'Section_1010310', 'blue')
-display_result(Xt, yt, 332, 'Section_1010309', 'red')
+display_result(Xt, yt, 332, 'Section_1010310', 'red')
 display_result(Xt, y_0311, 333, 'Section_1010311', 'blue')
-display_result(Xt, yt, 333, 'Section_1010309', 'red')
+display_result(Xt, yt, 333, 'Section_1010311', 'red')
 #以0310为训练集
 clf = svm.SVR(C=9.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1,
               gamma='auto_deprecated', kernel='rbf', max_iter=-1, shrinking=True,
@@ -62,9 +60,9 @@ yt = clf.predict(Xt)
 display_result(Xt, y, 334, 'Section_1010309', 'blue')
 display_result(Xt, yt, 334, 'Section_1010309', 'red')
 display_result(Xt, y_0310, 335, 'Section_1010310', 'blue')
-display_result(Xt, yt, 335, 'Section_1010309', 'red')
+display_result(Xt, yt, 335, 'Section_1010310', 'red')
 display_result(Xt, y_0311, 336, 'Section_1010311', 'blue')
-display_result(Xt, yt, 336, 'Section_1010309', 'red')
+display_result(Xt, yt, 336, 'Section_1010311', 'red')
 #以0311为训练集
 clf = svm.SVR(C=9.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1,
               gamma='auto_deprecated', kernel='rbf', max_iter=-1, shrinking=True,
@@ -74,9 +72,9 @@ yt = clf.predict(Xt)
 display_result(Xt, y, 337, 'Section_1010309', 'blue')
 display_result(Xt, yt, 337, 'Section_1010309', 'red')
 display_result(Xt, y_0310, 338, 'Section_1010310', 'blue')
-display_result(Xt, yt, 338, 'Section_1010309', 'red')
+display_result(Xt, yt, 338, 'Section_1010310', 'red')
 display_result(Xt, y_0311, 339, 'Section_1010311', 'blue')
-display_result(Xt, yt, 339, 'Section_1010309', 'red')
+display_result(Xt, yt, 339, 'Section_1010311', 'red')
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=1)
 plt.show()
 
